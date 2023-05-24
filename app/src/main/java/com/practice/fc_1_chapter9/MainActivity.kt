97efebb18cc9d7faced7a422798b8bc2033fc360
syb8200 <syb8200@naver.com>
2023-05-24 18:22:14 +0900

    viewBinding 설정 및 MediaPlayer 구현

--- app/src/main/java/com/practice/fc_1_chapter9/MainActivity.kt
+++ app/src/main/java/com/practice/fc_1_chapter9/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.practice.fc_1_chapter9
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.practice.fc_1_chapter9.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    private var mediaPlayer : MediaPlayer ?= null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.playButton.setOnClickListener {  mediaPlayerPlay() }
+        binding.pauseButton.setOnClickListener {  mediaPlayerPause() }
+        binding.stopButton.setOnClickListener {  mediaPlayerStop() }
+    }
+
+    private fun mediaPlayerPlay() {
+        if(mediaPlayer == null) {
+            mediaPlayer = MediaPlayer.create(this, R.raw.winner_winner_funky_chicken_dinner).apply {
+                isLooping = true
+            }
+        }
+        mediaPlayer?.start()
+    }
+
+    private fun mediaPlayerPause() {
+        mediaPlayer?.pause()
+    }
+
+    private fun mediaPlayerStop() {
+        mediaPlayer?.stop()
+
+        // 더이상 사용X -> 메모리에서 해제
+        mediaPlayer?.release()
+        mediaPlayer = null
     }
 }
